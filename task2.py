13e0f146e9fc1f4b5bac976c01cc2ba1d105c0d4
ainazik-netz <ainazik0807@gmail.com>
2019-10-25 12:47:13 +0600

    second commit

---
+++ task2.py
@@ -0,0 +1,12 @@
+text = input("Введите слова через пробел: ").split(" ")
+text.sort(key=len)
+print(" ".join(text))
+
+
+# this_method = 'orozbaeva ainazik tolkunovna'
+# print(this_method.split('a'))
+   
+
+# s = ['43','34','56']
+# # print(''.join(s))
+# print('//'.join(s))
